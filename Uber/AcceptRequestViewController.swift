1f061133f1a0991724916e9805c3df3bc40a210c
Robert Jackson Jr <jacksor@bgsu.edu>
2018-06-17 18:54:22 -0400

    Drivers can now see riders location and track how far away they are and how long it would take to reach their destination.

---
+++ Uber/AcceptRequestViewController.swift
@@ -0,0 +1,70 @@
+//
+//  AcceptRequestViewController.swift
+//  Uber
+//
+//  Created by Robert Jackson Jr on 6/17/18.
+//  Copyright © 2018 Robert Jackson Jr. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import FirebaseDatabase
+
+class AcceptRequestViewController: UIViewController {
+
+    
+    @IBOutlet var map: MKMapView!
+    
+    var requestLocation = CLLocationCoordinate2D()
+    var driverLocation = CLLocationCoordinate2D()
+    var requestEmail = ""
+    
+    
+    @IBAction func acceptTapped(_ sender: Any) {
+        //Update the ride request
+        
+        Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: requestEmail).observe(.childAdded) { (snapshot) in
+            snapshot.ref.updateChildValues(["driverLat":self.driverLocation.latitude, "driverLon":self.driverLocation.longitude])
+            Database.database().reference().child("RideRequests").removeAllObservers()
+
+        }
+        
+        
+        //Give directions
+        
+        let requestCLLocation = CLLocation(latitude: requestLocation.latitude, longitude: requestLocation.longitude)
+        
+        CLGeocoder().reverseGeocodeLocation(requestCLLocation) { (placemarks, error) in
+            if let placemarks = placemarks {
+                if placemarks.count > 0 {
+                    let placemark = MKPlacemark(placemark: placemarks[0])
+                    let mapItem = MKMapItem(placemark: placemark)
+                    mapItem.name = self.requestEmail
+                    let options = [MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving]
+                    mapItem.openInMaps(launchOptions: options)
+                    
+                }
+            }
+        }
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let region = MKCoordinateRegion(center: requestLocation, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        map.setRegion(region, animated: false)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = requestLocation
+        annotation.title = requestEmail
+        map.addAnnotation(annotation)
+        
+        
+    }
+
+   
+
+
+}
--- Uber/DriverTableViewController.swift
+++ Uber/DriverTableViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import MapKit
 
-class DriverTableViewController: UITableViewController {
+class DriverTableViewController: UITableViewController, CLLocationManagerDelegate {
 
     
     @IBAction func logoutTapped(_ sender: Any) {
@@ -19,33 +20,98 @@ class DriverTableViewController: UITableViewController {
     }
     
     
+    var rideRequests : [DataSnapshot] = []
+    var locationManager = CLLocationManager()
+    var driverLocation = CLLocationCoordinate2D()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        Database.database().reference().child("RideRequests").observe(.childAdded) { (snapshot) in
+            self.rideRequests.append(snapshot)
+            self.tableView.reloadData()
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (timer) in
+            self.tableView.reloadData()
+        }
     }
 
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coord = manager.location?.coordinate {
+            driverLocation = coord
+        }
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        
-        return 50
+        return rideRequests.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "rideRequestCell", for: indexPath)
 
-        cell.textLabel?.text = "Hello"
+        let snapshot = rideRequests[indexPath.row]
+        if let rideRequestDictionary = snapshot.value as? [String:AnyObject] {
+            if let email = rideRequestDictionary["email"] as? String {
+                
+                if let lat = rideRequestDictionary["lat"] as? Double {
+                    if let lon = rideRequestDictionary["lon"] as? Double {
+                        
+                        let driverCLLocation = CLLocation(latitude: driverLocation.latitude, longitude: driverLocation.longitude)
+                        let riderCLLocation = CLLocation(latitude: lat, longitude: lon)
+                        let distance = driverCLLocation.distance(from: riderCLLocation) / 1000
+                        let roundedDistance = round(distance * 100) / 100
+                        
+                        
+                        
+                        cell.textLabel?.text = "\(email) - 5km away"
 
+                        
+                    }
+                }
+                
+            }
+        }
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let snapshot = rideRequests[indexPath.row]
+        performSegue(withIdentifier: "acceptSegue", sender: snapshot)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let acceptVC = segue.destination as? AcceptRequestViewController {
+            
+            if let snapshot = sender as? DataSnapshot {
+                
+                if let rideRequestDictionary = snapshot.value as? [String:AnyObject] {
+                    if let email = rideRequestDictionary["email"] as? String {
+                        
+                        if let lat = rideRequestDictionary["lat"] as? Double {
+                            if let lon = rideRequestDictionary["lon"] as? Double {
+                                acceptVC.requestEmail = email
+                                
+                                let location = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                                acceptVC.requestLocation = location
+                                acceptVC.driverLocation = driverLocation
 
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
