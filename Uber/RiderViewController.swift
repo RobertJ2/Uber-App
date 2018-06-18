f57fa8d1739008239d29def8286238443b7bcdf1
Robert Jackson Jr <jacksor@bgsu.edu>
2018-06-17 21:15:58 -0400

    Driver can accept requests. Can now see the distance between driver and rider. Can pick fastest route to destination.

--- Uber/RiderViewController.swift
+++ Uber/RiderViewController.swift
@@ -20,6 +20,9 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
     var userLocation = CLLocationCoordinate2D()
     var uberHasBeenCalled = false
+    var driverOnTheWay = false
+    var driverLocation = CLLocationCoordinate2D()
+    
     
     
     override func viewDidLoad() {
@@ -36,21 +39,93 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
                 self.callAnUberButton.setTitle("Cancel Uber", for: .normal)
                 
                 Database.database().reference().child("RideRequests").removeAllObservers()
+                
+                if let rideRequestDictionary = snapshot.value as? [String:AnyObject] {
+                    if let driverLat = rideRequestDictionary["driverLat"] as? Double {
+                        if let driverLon = rideRequestDictionary["driverLon"] as? Double {
+                            self.driverLocation = CLLocationCoordinate2D(latitude: driverLat, longitude: driverLon)
+                            
+                            self.driverOnTheWay = true
+                            self.displayDriverAndRider()
+                            
+                            
+                            
+                            if let email = Auth.auth().currentUser?.email {
+                                Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childChanged) { (snapshot) in
+                                    if let rideRequestDictionary = snapshot.value as? [String:AnyObject] {
+                                        if let driverLat = rideRequestDictionary["driverLat"] as? Double {
+                                            if let driverLon = rideRequestDictionary["driverLon"] as? Double {
+                                                self.driverLocation = CLLocationCoordinate2D(latitude: driverLat, longitude: driverLon)
+                                                
+                                                self.driverOnTheWay = true
+                                                self.displayDriverAndRider()
+                                                
+                                            }
+                                        }
+                                    }
+                                    
+                                }
+                                
+                            }
+                            
+                    }
+                    }
+                }
+                
             })
         }
     }
+    
+    func displayDriverAndRider() {
+        let driverCLLocation = CLLocation(latitude: driverLocation.latitude, longitude: driverLocation.longitude)
+        let riderCLLocation = CLLocation(latitude: userLocation.latitude, longitude: userLocation.longitude)
+        let distance = driverCLLocation.distance(from: riderCLLocation) / 1000
+        let roundedDistance = round(distance * 100) / 100
+        callAnUberButton.setTitle("Your driver is \(roundedDistance)km away!", for: .normal)
+        map.removeAnnotations(map.annotations)
+        
+        let latDelta = abs(driverLocation.latitude - userLocation.latitude) * 2 + 0.005
+        let lonDelta = abs(driverLocation.longitude - userLocation.longitude) * 2 + 0.005
+
+        
+        let region = MKCoordinateRegion(center: userLocation, span: MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta))
+        map.setRegion(region, animated: true)
+        
+        let riderAnno = MKPointAnnotation()
+        riderAnno.coordinate = userLocation
+        riderAnno.title = "Your Location"
+        map.addAnnotation(riderAnno)
+        
+        let driverAnno = MKPointAnnotation()
+        driverAnno.coordinate = driverLocation
+        driverAnno.title = "Your Driver's Location"
+        map.addAnnotation(driverAnno)
+        
+       
+    }
+    
+    
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let coord = manager.location?.coordinate {
             let center = CLLocationCoordinate2D(latitude: coord.latitude, longitude: coord.longitude)
             userLocation = center
-            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-            map.setRegion(region, animated: true)
-            map.removeAnnotations(map.annotations)
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = center
-            annotation.title = "Your Location"
-            map.addAnnotation(annotation)
+            
+            
+            
+            if uberHasBeenCalled {
+                displayDriverAndRider()
+            } else {
+                
+                let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+                map.setRegion(region, animated: true)
+                map.removeAnnotations(map.annotations)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = center
+                annotation.title = "Your Location"
+                map.addAnnotation(annotation)
+            }
+            
         }
     }
 
@@ -64,6 +139,8 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func callAnUberTapped(_ sender: Any) {
+        
+        if !driverOnTheWay {
        
         if let email = Auth.auth().currentUser?.email {
             
@@ -81,7 +158,7 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
                 callAnUberButton.setTitle("Cancel Uber", for: .normal)
             }
         
-        
+            }
         }
     }
     
